2b21b694a5f2b45ddbd36149ae4bbfc98023934a
yuninze <gajeongsick.gmail.com>
2022-11-28 20:24:29 +0900

    jang-nan

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import discord
-
 import random
 from collections import Counter
 
@@ -9,21 +8,31 @@ intents=discord.Intents.default()
 intents.message_content=True
 client=discord.Client(intents=intents)
 
-@client.event
-async def on_ready():
-    print(f"로그인함 {client.user}")
+brand=["맥","켚엪씌","노","벅어킹","썹"]
+brands=[]
+users=[]
 
 @client.event
 async def on_message(message):
-    hmbg_brands=["맥 도널드","케이 에프 씨","버거 킹","썹","노 브랜드"]
-    hmbg_brands_chosen_cache=[]
-    if message.author==client.user:
-        return
-    if message.content=="/햄버거":
-        hmbg_chosen:str=random.choice(hmbg_brands)
-        hmbg_brands_chosen_cache.append(hmbg_choice)
-        hmbg_brands_chosen_counts:dict=Counter(hmbg_brands_chosen_cache)
+    if message.content=="/햄":
+        if not len(users)==3:
+            if not message.author in users:
+                brands_chosen=random.choice(brand)
+                brands.append(brands_chosen)
+                brands_count:dict=Counter(brands)
+                users.append(message.author)
+                await message.channel.send(
+                    f"햄버거::{brands_chosen} ({brands_count[brands_chosen]}번 나옴)")
+            else:
+                await message.channel.send(
+                    f"{message.author}는 이미 투표함")
+        else:
+            await message.channel.send(
+                f"{len(users)}")
+    elif message.content=="/투":
         await message.channel.send(
-            f"햄버거::{chosen} (지금까지 {hmbg_brands_chosen_counts[chosen]}번 나왔음)")
-        
+            f'''장전된 샌드위치 브랜드 {", ".join(brand)}... {len(brand)}개''')
+    elif message.content=="/누":
+        await message.channel.send(
+            f'''참여자 {len(users)}명''')
 client.run(token)
